127a3e66cd25904124ac3812cb4cee4bf9bd385d
Corey Mutnik <cmutnik@hawaii.edu>
2020-05-04 14:50:49 -0400

    quick push

---
+++ AI_ML_DeepLearning/Deep_Learning_Prerequisites_Linear_regression_in_Python/README.md
@@ -0,0 +1,2 @@
+Linear Regression Class can be found at the following URL:
+https://bah.udemy.com/course/data-science-linear-regression-in-python/learn/lecture/9057426?start=180#overview
---
+++ AI_ML_DeepLearning/Deep_Learning_Prerequisites_Logistic_Regression/l1_regularization.py
@@ -0,0 +1,42 @@
+# Python script to demonstrate use of l1 regularization
+#   generate data that has a large matrix as its input
+#   y only depends on a few features from the data, the rest is just noise
+#   use l1 regularization to find a sparse set of weights that id the useful dimensions of x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
